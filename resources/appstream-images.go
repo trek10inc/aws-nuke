1450086131ec31b960df05f2bb7dd635d7b88345
Michael Barney <mbarney@trek10.com>
2024-08-09 12:12:17 -0400

    feat(AppStreamImage): add additional properties

--- resources/appstream-images.go
+++ resources/appstream-images.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/appstream"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
 type AppStreamImage struct {
 	svc        *appstream.AppStream
 	name       *string
 	visibility *string
+	inUse      *bool
 }
 
 func init() {
@@ -24,6 +27,46 @@ func ListAppStreamImages(sess *session.Session) ([]Resource, error) {
 
 	params := &appstream.DescribeImagesInput{}
 
+	imagesInUse := make(map[string]*bool)
+
+	// mark which images are in use by fleets, image builders
+	fleetParams := &appstream.DescribeFleetsInput{}
+	for {
+		output, err := svc.DescribeFleets(fleetParams)
+		if err != nil {
+			return nil, err
+		}
+		for _, fleet := range output.Fleets {
+			inUse := true
+			if fleet.ImageArn != nil {
+				imagesInUse[*fleet.ImageArn] = &inUse
+			}
+		}
+
+		if output.NextToken == nil {
+			break
+		}
+		fleetParams.NextToken = output.NextToken
+	}
+
+	imageBuilderParams := &appstream.DescribeImageBuildersInput{
+		MaxResults: aws.Int64(100),
+	}
+	for {
+		output, err := svc.DescribeImageBuilders(imageBuilderParams)
+		if err != nil {
+			return nil, err
+		}
+		for _, imageBuilder := range output.ImageBuilders {
+			inUse := true
+			imagesInUse[*imageBuilder.ImageArn] = &inUse
+		}
+		if output.NextToken == nil {
+			break
+		}
+		imageBuilderParams.NextToken = output.NextToken
+	}
+
 	for {
 		output, err := svc.DescribeImages(params)
 		if err != nil {
@@ -34,6 +77,7 @@ func ListAppStreamImages(sess *session.Session) ([]Resource, error) {
 				svc:        svc,
 				name:       image.Name,
 				visibility: image.Visibility,
+				inUse:      imagesInUse[*image.Arn],
 			})
 		}
 		if output.NextToken == nil {
@@ -52,6 +96,14 @@ func (f *AppStreamImage) Remove() error {
 	})
 
 	return err
+}
+
+func (f *AppStreamImage) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("Name", f.name)
+	properties.Set("Visibility", f.visibility)
+	properties.Set("InUse", f.inUse)
+	return properties
 }
 
 func (f *AppStreamImage) String() string {
