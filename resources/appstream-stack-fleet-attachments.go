8c65daaac44ee4cb12058bb6cf926a9378874245
Michael Barney <mbarney@trek10.com>
2024-08-09 15:31:40 -0400

    feat(AppStreamStackFleetAttachment): add additional properties and tags

--- resources/appstream-stack-fleet-attachments.go
+++ resources/appstream-stack-fleet-attachments.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/appstream"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
 type AppStreamStackFleetAttachment struct {
 	svc       *appstream.AppStream
 	stackName *string
 	fleetName *string
+	stackTags map[string]*string
+	fleetTags map[string]*string
 }
 
 func init() {
@@ -29,9 +32,7 @@ func ListAppStreamStackFleetAttachments(sess *session.Session) ([]Resource, erro
 			return nil, err
 		}
 
-		for _, stack := range output.Stacks {
-			stacks = append(stacks, stack)
-		}
+		stacks = append(stacks, output.Stacks...)
 
 		if output.NextToken == nil {
 			break
@@ -50,10 +51,33 @@ func ListAppStreamStackFleetAttachments(sess *session.Session) ([]Resource, erro
 		}
 
 		for _, name := range output.Names {
+			describeFleetsResponse, err := svc.DescribeFleets(&appstream.DescribeFleetsInput{
+				Names: []*string{name},
+			})
+			if err != nil {
+				return nil, err
+			}
+			fleetArn := describeFleetsResponse.Fleets[0].Arn
+			listStackTagsParams := &appstream.ListTagsForResourceInput{
+				ResourceArn: stack.Arn,
+			}
+			stackTags, err := svc.ListTagsForResource(listStackTagsParams)
+			if err != nil {
+				return nil, err
+			}
+			listFleetTagsParams := &appstream.ListTagsForResourceInput{
+				ResourceArn: fleetArn,
+			}
+			fleetTags, err := svc.ListTagsForResource(listFleetTagsParams)
+			if err != nil {
+				return nil, err
+			}
 			resources = append(resources, &AppStreamStackFleetAttachment{
 				svc:       svc,
 				stackName: stack.Name,
 				fleetName: name,
+				stackTags: stackTags.Tags,
+				fleetTags: fleetTags.Tags,
 			})
 		}
 	}
@@ -69,6 +93,19 @@ func (f *AppStreamStackFleetAttachment) Remove() error {
 	})
 
 	return err
+}
+
+func (f *AppStreamStackFleetAttachment) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("StackName", f.stackName)
+	properties.Set("FleetName", f.fleetName)
+	for key, val := range f.stackTags {
+		properties.SetTagWithPrefix("stack", &key, val)
+	}
+	for key, val := range f.fleetTags {
+		properties.SetTagWithPrefix("fleet", &key, val)
+	}
+	return properties
 }
 
 func (f *AppStreamStackFleetAttachment) String() string {
