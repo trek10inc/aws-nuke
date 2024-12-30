e17e5d290c6a73a6b375678dc6beb5daebfebaa4
Michael Barney <mbarney@trek10.com>
2024-12-30 09:59:08 -0500

    feat(iam-saml-provider): add support SAML Provider tags

--- resources/iam-saml-provider.go
+++ resources/iam-saml-provider.go
@@ -3,11 +3,13 @@ package resources
 import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
 type IAMSAMLProvider struct {
-	svc *iam.IAM
-	arn string
+	svc  *iam.IAM
+	arn  string
+	tags []*iam.Tag
 }
 
 func init() {
@@ -25,9 +27,17 @@ func ListIAMSAMLProvider(sess *session.Session) ([]Resource, error) {
 	}
 
 	for _, out := range resp.SAMLProviderList {
+		params := &iam.GetSAMLProviderInput{
+			SAMLProviderArn: out.Arn,
+		}
+		resp, err := svc.GetSAMLProvider(params)
+		if err != nil {
+			return nil, err
+		}
 		resources = append(resources, &IAMSAMLProvider{
-			svc: svc,
-			arn: *out.Arn,
+			svc:  svc,
+			arn:  *out.Arn,
+			tags: resp.Tags,
 		})
 	}
 
@@ -47,4 +57,15 @@ func (e *IAMSAMLProvider) Remove() error {
 
 func (e *IAMSAMLProvider) String() string {
 	return e.arn
+}
+
+func (e *IAMSAMLProvider) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("Arn", e.arn)
+
+	for _, tag := range e.tags {
+		properties.SetTag(tag.Key, tag.Value)
+	}
+
+	return properties
 }
